ed1c24e863b75e279187bca96967ca232063fc0e
Simmi Sharma <i.simmi.sharma@gmail.com>
2023-01-21 00:15:47 +0530

    Adding all files I learned while practicing techonologies like python,javascript,css and html

---
+++ learn/javascript/1.js
@@ -0,0 +1,8 @@
+function changeSomething(){
+    let x = document.getElementById("idOne");
+    let y = document.getElementsByTagName("li");
+
+    x.innerHTML="My favourite Fruits";
+    y[1].innerHTML ="Banana";
+   // let z = document.getElementById("xyz").style.color="red";
+}
